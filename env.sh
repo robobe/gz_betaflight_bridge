c9bb449d484b1166e6db8e4edbe256b9106fce42
robobe <robobe2020@gmail.com>
2026-09-01 05:55:46 +0300

    bash prompt

--- env.sh
+++ env.sh
@@ -2,3 +2,6 @@
 
 alias gzps="pgrep -af '^gz sim (server|gui)'"
 alias gzkill="pkill -9 -f '^gz sim (server|gui)'"
+
+source /usr/lib/git-core/git-sh-prompt
+PS1='\[\e[32m\]\u@\h\[\e[0m\]:\[\e[34m\]\w\[\e[33m\]$(__git_ps1 " (%s)")\[\e[0m\]\$ '
