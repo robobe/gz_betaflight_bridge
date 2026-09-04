6b47b12bd05be4fb8b6838bd32e7bc005b55239e
robobe <robobe2020@gmail.com>
2026-09-04 06:55:40 +0300

    fix alias gzxx command

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-alias gzps="pgrep -af '^gz sim (server|gui)'"
-alias gzkill="pkill -9 -f '^gz sim (server|gui)'"
+alias gzps="pgrep -af '^gz sim( |$)'"
+alias gzkill="pkill -9 -f '^gz sim( |$)'"
 
 source /usr/lib/git-core/git-sh-prompt
 PS1='\[\e[32m\]\u@\h\[\e[0m\]:\[\e[34m\]\w\[\e[33m\]$(__git_ps1 " (%s)")\[\e[0m\]\$ '
